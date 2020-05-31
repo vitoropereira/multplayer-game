99a26a85479b5a6e985a5ca3dbe0449838e29605
VITOR PEREIRA <vop1234@hotmail.com>
2020-05-31 07:50:54 -0300

    Fatoring

---
+++ game.js
@@ -0,0 +1,100 @@
+export default function createGame() {
+  const state = {
+    players: {},
+    fruits: {}
+  }
+
+  function addPlayer(command) {
+    const playerId = command.playerId
+    const playerX = command.playerX
+    const playerY = command.playerY
+
+    state.players[playerId] = {
+      x: playerX,
+      y: playerY
+    }
+  }
+
+  function removePlayer(command) {
+    const playerId = command.playerId
+
+    delete state.players[playerId]
+  }
+
+  function addFruit(command) {
+    const fruitId = command.fruitId
+    const fruitX = command.fruitX
+    const fruitY = command.fruitY
+
+    state.fruits[fruitId] = {
+      x: fruitX,
+      y: fruitY
+    }
+  }
+
+  function removeFruit(command) {
+    const fruitId = command.fruitId
+
+    delete state.fruits[fruitId]
+  }
+
+  function movePlayer(command) {
+    const acceptedMoves = {
+      ArrowUp(player) {
+        if (player.y - 1 >= 0) {
+          player.y = player.y - 1
+        }
+      },
+      ArrowRight(player) {
+        if (player.x + 1 < screen.width) {
+          player.x = player.x + 1
+        }
+      },
+      ArrowDown(player) {
+        if (player.y + 1 < screen.height) {
+          player.y = player.y + 1
+        }
+      },
+      ArrowLeft(player) {
+        if (player.x - 1 >= 0) {
+          player.x = player.x - 1
+        }
+      },
+
+    }
+
+    const keyPressed = command.keyPressed
+    const playerId = command.playerId
+    const player = state.players[command.playerId]
+    const moveFunctions = acceptedMoves[keyPressed]
+
+    if (player && moveFunctions) {
+      moveFunctions(player)
+      checkForFruitCollision(playerId)
+    }
+
+
+    return
+  }
+
+  function checkForFruitCollision(playerId) {
+    const player = state.players[playerId]
+
+    for (const fruitId in state.fruits) {
+      const fruit = state.fruits[fruitId]
+      if (player.x === fruit.x && player.y === fruit.y) {
+
+        removeFruit({ fruitId })
+      }
+    }
+  }
+
+  return {
+    addPlayer,
+    removePlayer,
+    addFruit,
+    removeFruit,
+    movePlayer,
+    state
+  }
+}
